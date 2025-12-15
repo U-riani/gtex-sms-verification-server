fa3c8570be2ace8a1681f95b8a3b0766e76d0ffa
U-riani <sandropapiashvili@gmail.com>
2025-12-15 17:57:12 +0400

    sdsdsd

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ import userRoutes from "./routes/userRoutes.js";
 connectDB();
 
 const app = express();
+// HARD STOP for preflight requests
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,DELETE,OPTIONS"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 // Configure CORS
 app.use(
