3add9b748f60d927dd743c1e24f8841f0eeb82b7
U-riani <sandropapiashvili@gmail.com>
2026-03-11 13:27:23 +0400

    s

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get("/", (req, res) => {
   res.json({ message: "title2" });
 });
 
-// Error handler
+// // Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
